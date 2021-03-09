27081d9bb9e18e0e780fd42b26fc91de4a631b77
Ilyagu <inagdimaev@mail.ru>
2021-03-09 15:51:06 +0300

    added IT1

--- IT1/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ilyagu/Advanced_programming_C/IT1
-# Build directory: /home/ilyagu/Advanced_programming_C/IT1/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
